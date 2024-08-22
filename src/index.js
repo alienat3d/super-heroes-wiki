1afc5d0772c04f0f9e023cf6fbfbe21af12b7596
Al Zaplin <zaplin.dev@gmail.com>
2024-08-22 18:41:47 +0300

    fix(styles) Card title padding

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import './style/index.scss';
 import device from "current-device";
 
 import { hintForMobileFunc } from "./modules/hint-for-mobile";
-import { showInfoFunc } from "./modules/show-info";
 import { loadFunc } from "./modules/load";
 import { filterFunc } from "./modules/filter";
 
 hintForMobileFunc();
-showInfoFunc();
 loadFunc();
 filterFunc();
